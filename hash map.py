aa7de8aa132dbbe543a2626a675a36a284b1ce1f
sunnysunita <sunnysunita.com@gmail.com>
2020-05-17 23:57:06 +0530

    final2.0

--- hash map.py
+++ hash map.py
@@ -1,22 +1,24 @@
-
 class MapNode:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
+
 class Map:
     def __init__(self):
-        self.bucket_size = 10
-        self.bucket = [None for i in range(self.bucket_size)]
+        self.bucketsize = 10
+        self.bucket = [None for i in range(self.bucketsize)]
         self.count = 0
-
 
     def size(self):
         return self.count
 
-    def get_index(self,key):
-        return abs(hash(key))%self.bucket_size
+
+    def get_index(self, key):
+        return abs(hash(key)) % self.bucketsize
+
 
     def insert(self, key, value):
         index = self.get_index(key)
@@ -26,34 +28,13 @@ class Map:
                 head.value = value
                 return
             head = head.next
-
         head = self.bucket[index]
         new_node = MapNode(key, value)
         new_node.next = head
-        self.bucket[index] = new_node
+        head = new_node
+        self.bucket[index] = head
         self.count += 1
-
-    def remove(self, key):
-        index = self.get_index(key)
-        head = self.bucket[index]
-        if head.key == key:
-            x = head.value
-            head = head.next
-            self.bucket[index] = head
-            self.count -= 1
-            return x
-        pre = None
-        temp = head
-        while head is not None:
-            if head.key == key:
-                pre.next = head.next
-                head.next = None
-                self.bucket[index] = temp
-                self.count -= 1
-                return head.value
-            pre = head
-            head = head.next
-        return False
+        return
 
     def search(self, key):
         index = self.get_index(key)
@@ -63,20 +44,40 @@ class Map:
                 return head.value
             head = head.next
         return None
-m = Map()
-print(m.size())
-m.insert("sunny", 79)
-print(m.size())
-m.insert("arun", 80)
-print(m.size())
-m.insert("arun", 100)
-print(m.size())
-m.insert("ram", 800)
-m.insert("shyam", 180)
-m.insert("varun", 980)
-m.insert("satyam", 80)
-print(m.size())
-print(m.search("sunny"))
-print(m.remove("sunny"))
-print(m.search("sunny"))
 
+    def remove(self, key):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        pre = None
+        while head is not None:
+            if head.key == key:
+                if pre == None:
+                    head = head.next
+                    self.bucket[index] = head
+                    self.count -= 1
+                    return True
+                else:
+                    pre = head.next
+                    head.next = None
+                    self.count -= 1
+                    return True
+            pre = head
+            head = head.next
+        return False
+
+
+m = Map()
+m.insert("abc", 100)
+m.insert("abd", 200)
+m.insert("abe", 300)
+m.insert("abf", 400)
+m.insert("abg", 500)
+m.insert("abh", 600)
+m.insert("abi", 700)
+print(m.size())
+print(m.search("abi"))
+print(m.remove("abi"))
+print(m.search("abi"))
+print(m.size())
+print(m.insert("abc",1000))
+print(m.size())
--- rough.py
+++ rough.py
@@ -1,1 +1,83 @@
-print(-0)
+class MapNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+
+class Map:
+    def __init__(self):
+        self.bucketsize = 10
+        self.bucket = [None for i in range(self.bucketsize)]
+        self.count = 0
+
+    def size(self):
+        return self.count
+
+
+    def get_index(self, key):
+        return abs(hash(key)) % self.bucketsize
+
+
+    def insert(self, key, value):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        while head is not None:
+            if head.key == key:
+                head.value = value
+                return
+            head = head.next
+        head = self.bucket[index]
+        new_node = MapNode(key, value)
+        new_node.next = head
+        head = new_node
+        self.bucket[index] = head
+        self.count += 1
+        return
+
+    def search(self, key):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        while head is not None:
+            if head.key == key:
+                return head.value
+            head = head.next
+        return None
+
+    def remove(self, key):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        pre = None
+        while head is not None:
+            if head.key == key:
+                if pre == None:
+                    head = head.next
+                    self.bucket[index] = head
+                    self.count -= 1
+                    return True
+                else:
+                    pre = head.next
+                    head.next = None
+                    self.count -= 1
+                    return True
+            pre = head
+            head = head.next
+        return False
+
+
+m = Map()
+m.insert("abc", 100)
+m.insert("abd", 200)
+m.insert("abe", 300)
+m.insert("abf", 400)
+m.insert("abg", 500)
+m.insert("abh", 600)
+m.insert("abi", 700)
+print(m.size())
+print(m.search("abi"))
+print(m.remove("abi"))
+print(m.search("abi"))
+print(m.size())
+print(m.insert("abc",1000))
+print(m.size())
