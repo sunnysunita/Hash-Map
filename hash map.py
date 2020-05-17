674fe4e809a456d8ea998462ea4bae950db7cde3
sunnysunita <sunnysunita.com@gmail.com>
2020-05-17 19:48:04 +0530

    final

--- hash map.py
+++ hash map.py
@@ -32,6 +32,34 @@ class Map:
         self.bucket[index] = new_node
         self.count += 1
 
+    def remove(self, key):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        if head.key == key:
+            x = head.value
+            head = head.next
+            self.bucket[index] = head
+            return x
+        pre = None
+        temp = head
+        while head is not None:
+            if head.key == key:
+                pre.next = head.next
+                head.next = None
+                self.bucket[index] = temp
+                return head.value
+            pre = head
+            head = head.next
+        return False
+
+    def search(self, key):
+        index = self.get_index(key)
+        head = self.bucket[index]
+        while head is not None:
+            if head.key == key:
+                return head.value
+            head = head.next
+        return None
 m = Map()
 print(m.size())
 m.insert("sunny", 79)
@@ -40,3 +68,12 @@ m.insert("arun", 80)
 print(m.size())
 m.insert("arun", 100)
 print(m.size())
+m.insert("ram", 800)
+m.insert("shyam", 180)
+m.insert("varun", 980)
+m.insert("satyam", 80)
+print(m.size())
+print(m.search("sunny"))
+print(m.remove("sunny"))
+print(m.search("sunny"))
+
